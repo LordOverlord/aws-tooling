973200c2e2139dc84551ea58fdba25e260be0b0c
Rafael <barto_et@hotmail.com>
2022-03-17 08:10:19 -0600

    delete ebs v1

--- delete-ebs.sh
+++ delete-ebs.sh
@@ -1,8 +1,14 @@
 #!bin/zsh
+AWS_PROFILE='okta'
 dryrun=$1
 if [ -z "$dryrun" ]
 then
 dryrun='--dry-run'
 fi
 
+while read line
+do
+    echo "Deleting EBS: $line"
+    aws ec2 delete-volume --volume-id $line $dryrun
+done < ebs.csv
 echo $dryrun
