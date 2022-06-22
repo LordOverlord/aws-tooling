dae1d8aaa2227806f5accb114ca9099450210c49
Rafael <barto_et@hotmail.com>
2022-06-21 21:10:25 -0500

    new comment

--- get-ebs.sh
+++ get-ebs.sh
@@ -1,3 +1,4 @@
+#Some coment
 AWS_PROFILE = okta
 filters = 'Name=status,Values=available'
 loggin = aws ec2 describe-volumes --filters $filters --profile okta --query "Volumes[*].{ID:VolumeId,Size:Size}" --output text
