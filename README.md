b7081f02324d341cba4309c2ca4087680c9a4256
Rafael <barto_et@hotmail.com>
2022-07-15 20:36:36 -0500

    readme and sh improved

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# aws-tooling
+# Tooling for AWS Maintenance
+
+## Table
+
+### Prerequisites
+
+
+
+### Powershell - Completed
+
+
+
+### Born Against SHell - 50% 
+
+Edit the following lines inside get-ebs.sh
+
+```bash
+
+```
+### Python - Initializing
+
--- get-ebs.sh
+++ get-ebs.sh
@@ -1,6 +1,13 @@
-#Some coment
-#AWS_PROFILE = okta
-export filters = 'Name=status,Values=available'
-loggin =($(aws ec2 describe-volumes --filters $filters --profile okta --query "Volumes[*].{ID:VolumeId,Size:Size}" --output text))
 
-echo $loggin
+#some AWS Variables
+export AWS_SAHRED_CREDENTIAL_FILE="~/.aws/credential"
+export AWS_REGION="us-east-1"
+export AWS_PROFILE="905467511563-bu-torq-poweruser"
+#some variables to compose the query
+export aprofile = "905467511563-bu-torq-poweruser"
+export filters = 'Name=status,Values=available'
+export aregion = "us-east-1"
+#getting the info in text format
+loggin=($(aws ec2 describe-volumes --filters $filters --profile $aprofile --region $aregion --query "Volumes[*].{ID:VolumeId,Size:Size}" --output text))
+#exporting the data to csv
+echo $loggin > 
